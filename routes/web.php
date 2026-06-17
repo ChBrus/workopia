d998cad1aed904604c60201cea029386aa4bd54f
ChBrus <cbruno.carrillo@gmail.com>
2026-06-16 22:57:44 -0600

    Route Params and Query Params

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,10 @@ Route::get('/jobs', function () {
     return '<h1>Avaialable jobs</h1>';
 })->name('jobs');
 
-Route::post('/submit', function () {
-    return 'Submitted';
-});
+Route::get('/posts/{id}', function (string $id) {
+    return "Post ID: $id";
+})->whereNumber('id');
 
-Route::any('/submit', function () {
-    return 'Match submitted';
-});
-
-Route::get('/test', function () {
-    $url = route('jobs');
-    return "<a href='$url'>Jobs</a>";
-});
-
-Route::get('/api/users', function () {
-    return [
-        'name' => 'John Doe',
-        'email' => 'john.doe@example.com',
-    ];
+Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return "Post ID: $id, Comment ID: $commentId";
 });
