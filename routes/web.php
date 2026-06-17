6776919b883e7d8e524643e46e4cb42b2dc28a09
ChBrus <cbruno.carrillo@gmail.com>
2026-06-16 22:38:11 -0600

    Introduction to routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return '<h1>Avaialable jobs</h1>';
+})->name('jobs');
+
+Route::post('/submit', function () {
+    return 'Submitted';
+});
+
+Route::any('/submit', function () {
+    return 'Match submitted';
+});
+
+Route::get('/test', function () {
+    $url = route('jobs');
+    return "<a href='$url'>Jobs</a>";
+});
+
+Route::get('/api/users', function () {
+    return [
+        'name' => 'John Doe',
+        'email' => 'john.doe@example.com',
+    ];
+});
