e88843f84465cf318d547162f40b078b7c984081
ChBrus <cbruno.carrillo@gmail.com>
2026-06-17 23:17:05 -0600

    Request Object and Query Params

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,10 +11,23 @@ Route::get('/jobs', function () {
     return '<h1>Avaialable jobs</h1>';
 })->name('jobs');
 
-Route::get('/posts/{id}', function (string $id) {
-    return "Post ID: $id";
-})->whereNumber('id');
+Route::get('/test', function (Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
+});
 
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return "Post ID: $id, Comment ID: $commentId";
+Route::get('/users', function (Request $request) {
+    // return $request->query('name');
+    // return $request->only(['name', 'age']);
+    // return $request->all();
+    // return $request->has('name');
+    // return $request->input('name', 'Default Name');
+    return $request->except(['name']);
 });
