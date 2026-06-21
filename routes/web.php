da5271b88bd5b1bded28640b6bc2938b2018d827
ChBrus <cbruno.carrillo@gmail.com>
2026-06-20 23:09:59 -0600

    Create and Display Views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,33 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return '<h1>Avaialable jobs</h1>';
+    return view('jobs.index');
 })->name('jobs');
 
-Route::get('/test', function () {
-    return response('<h1>Hello World</h1>', 200)
-        ->header('Content-Type', 'text/plain');
-});
-
-Route::get('/test-json', function () {
-    return response()->json([
-        'name' => 'John Doe',
-        'age' => 20,
-    ])->cookie('name', 'John Doe');
-});
-
-Route::get('/read-cookie', function (Request $request) {
-    $cookie_value = $request->cookie('name');
-
-    return response()->json([
-        'cookie_value' => $cookie_value,
-    ]);
-});
-
-Route::get('/download', function () {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get('/notfound', function () {
-    return new Response('Page Not Found', 404);
-});
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
